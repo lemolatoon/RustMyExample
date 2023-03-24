58dda5c86e6701625a28c69939f8adc624d17996
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-03-25 01:05:33 +0900

    Add trait_object

---
+++ trait_object/src/main.rs
@@ -0,0 +1,26 @@
+pub trait Draw {
+    fn draw(&self) {}
+}
+
+pub struct A(usize);
+pub struct B(isize);
+
+impl Draw for A {}
+impl Draw for B {}
+
+pub struct Screen1 {
+    pub components: Vec<Box<dyn Draw>>, // <---> impl Draw
+}
+
+pub struct Screen2<T: Draw> {
+    pub components: Vec<T>,
+}
+
+fn main() {
+    let screen1 = Screen1 {
+        components: vec![Box::new(A(1)), Box::new(B(2))],
+    };
+    let screen2 = Screen2 {
+        components: vec![A(1), B(2)],
+    };
+}
